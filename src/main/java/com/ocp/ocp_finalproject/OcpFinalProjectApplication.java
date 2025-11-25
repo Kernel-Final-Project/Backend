463f8fb86c6a5a423855e5bdb4ccba773e73d2d2
너의 이름 <@example.com>
2025-11-25 11:06:39 +0900

    Initial commit: Spring Boot + MySQL setup

---
+++ src/main/java/com/ocp/ocp_finalproject/OcpFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ocp.ocp_finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcpFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcpFinalProjectApplication.class, args);
+    }
+
+}
