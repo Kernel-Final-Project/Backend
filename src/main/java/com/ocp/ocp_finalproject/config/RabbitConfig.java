65adf2ef8801188ed877ec9aa234b1435c89d968
김준휘 <junikim11@gmail.com>
2025-11-27 18:46:53 +0900

    RabbitMQ 기반 콘텐츠 작업 발행/조회 API 추가

---
+++ src/main/java/com/ocp/ocp_finalproject/config/RabbitConfig.java
@@ -0,0 +1,34 @@
+package com.ocp.ocp_finalproject.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Value("${app.rabbit.queue.request}")
+    private String contentRequestQueueName;
+
+    @Bean
+    public Queue contentRequestQueue() {
+        return new Queue(contentRequestQueueName, true); // durable queue
+    }
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         JacksonJsonMessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+}
