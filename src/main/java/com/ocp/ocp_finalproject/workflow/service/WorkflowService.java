252b9077e608e05175f6731a545e0a974fc4135c
kodaeyoung <kody7111@naver.com>
2025-12-02 12:15:59 +0900

    fix:createWorkflow메서드에 스케줄러 예외 적용

--- src/main/java/com/ocp/ocp_finalproject/workflow/service/WorkflowService.java
+++ src/main/java/com/ocp/ocp_finalproject/workflow/service/WorkflowService.java
@@ -4,6 +4,7 @@ import com.ocp.ocp_finalproject.workflow.dto.WorkflowEditResponse;
 import com.ocp.ocp_finalproject.workflow.dto.WorkflowListResponse;
 import com.ocp.ocp_finalproject.workflow.dto.WorkflowRequest;
 import com.ocp.ocp_finalproject.workflow.dto.WorkflowResponse;
+import org.quartz.SchedulerException;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ public interface WorkflowService {
 
     List<WorkflowListResponse> findWorkflows(Long userId);
 
-    WorkflowResponse createWorkflow(Long userId, WorkflowRequest workflowRequest);
+    WorkflowResponse createWorkflow(Long userId, WorkflowRequest workflowRequest) throws SchedulerException;
 
     WorkflowEditResponse findWorkflow(Long workflowId, Long userId);
 }
