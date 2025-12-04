bbbcb7b7270e85a950633c5a785bdd09a061246a
kimjuno1220 <kjo9577@gmail.com>
2025-12-04 14:36:05 +0900

    fix: NoticeController에 ApiResponse 추가

--- src/main/java/com/ocp/ocp_finalproject/notice/domain/NoticeFile.java
+++ src/main/java/com/ocp/ocp_finalproject/notice/domain/NoticeFile.java
@@ -1,4 +1,4 @@
-package com.ocp.ocp_finalproject.notice.damain;
+package com.ocp.ocp_finalproject.notice.domain;
 
 import com.ocp.ocp_finalproject.common.entity.BaseEntity;
 import jakarta.persistence.*;
