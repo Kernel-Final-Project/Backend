2b5e27b46f603c2c241c5982572b4ed403f4dcb3
kimjuno1220 <kjo9577@gmail.com>
2025-12-05 17:46:43 +0900

    feat: 관리자 공지사항 삭제 기능 추가

--- src/main/java/com/ocp/ocp_finalproject/notice/api/admin/NoticeAdminController.java
+++ src/main/java/com/ocp/ocp_finalproject/notice/api/admin/NoticeAdminController.java
@@ -1,15 +1,12 @@
 package com.ocp.ocp_finalproject.notice.api.admin;
 
-import com.ocp.ocp_finalproject.common.response.ApiResponse;
+import com.ocp.ocp_finalproject.common.response.ApiResult;
 import com.ocp.ocp_finalproject.notice.dto.request.NoticeCreateRequest;
 import com.ocp.ocp_finalproject.notice.dto.response.NoticeResponse;
 import com.ocp.ocp_finalproject.notice.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,8 +19,18 @@ public class NoticeAdminController {
      * 공지사항 등록
      */
     @PostMapping
-    public ResponseEntity<ApiResponse<NoticeResponse>> createNotice(@RequestBody NoticeCreateRequest request) {
-        return ResponseEntity.ok(ApiResponse.success("공지사항 등록 성공", noticeService.createNotice(request))
+    public ResponseEntity<ApiResult<NoticeResponse>> createNotice(@RequestBody NoticeCreateRequest request) {
+        return ResponseEntity.ok(ApiResult.success("공지사항 등록 성공", noticeService.createNotice(request))
+        );
+    }
+
+    /**
+     * 공지사항 삭제
+     */
+    @DeleteMapping("/{noticeId}")
+    public ResponseEntity<ApiResult<Void>> deleteNotice(@PathVariable Long noticeId) {
+        noticeService.deleteNotice(noticeId);
+        return ResponseEntity.ok(ApiResult.success("공지사항 삭제 성공", null)
         );
     }
 }
--- src/main/java/com/ocp/ocp_finalproject/notice/api/user/NoticeController.java
+++ src/main/java/com/ocp/ocp_finalproject/notice/api/user/NoticeController.java
@@ -1,6 +1,6 @@
 package com.ocp.ocp_finalproject.notice.api.user;
 
-import com.ocp.ocp_finalproject.common.response.ApiResponse;
+import com.ocp.ocp_finalproject.common.response.ApiResult;
 import com.ocp.ocp_finalproject.notice.dto.response.NoticeResponse;
 import com.ocp.ocp_finalproject.notice.service.NoticeService;
 import lombok.RequiredArgsConstructor;
@@ -23,17 +23,17 @@ public class NoticeController {
      * 공지사항 목록 조회
      */
     @GetMapping
-    public ResponseEntity<ApiResponse<List<NoticeResponse>>> getAllNotices() {
-        return ResponseEntity.ok(ApiResponse.success("공지사항 목록 조회 성공", noticeService.getAllNotice()));
+    public ResponseEntity<ApiResult<List<NoticeResponse>>> getAllNotices() {
+        return ResponseEntity.ok(ApiResult.success("공지사항 목록 조회 성공", noticeService.getAllNotice()));
     }
     /**
      * 공지사항 상세 조회
      */
     @GetMapping("/{noticeId}")
-    public ResponseEntity<ApiResponse<NoticeResponse>> getNotice(
+    public ResponseEntity<ApiResult<NoticeResponse>> getNotice(
             @PathVariable Long noticeId
     ) {
-        return ResponseEntity.ok(ApiResponse.success("공지사항 조회 성공", noticeService.getNotice(noticeId)));
+        return ResponseEntity.ok(ApiResult.success("공지사항 조회 성공", noticeService.getNotice(noticeId)));
     }
 
 }
