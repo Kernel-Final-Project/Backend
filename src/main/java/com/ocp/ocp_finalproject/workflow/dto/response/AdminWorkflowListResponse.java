37773e493b55d25a3d8e1215cf684c3701f2a605
“Seoyoung-Kyung” <tjdud90352@gmail.com>
2025-12-16 12:21:08 +0900

    chore: 사용하지 않는 import 제거

--- src/main/java/com/ocp/ocp_finalproject/workflow/dto/response/AdminWorkflowListResponse.java
+++ src/main/java/com/ocp/ocp_finalproject/workflow/dto/response/AdminWorkflowListResponse.java
@@ -1,9 +1,6 @@
 package com.ocp.ocp_finalproject.workflow.dto.response;
 
-import com.ocp.ocp_finalproject.blog.domain.UserBlog;
-import com.ocp.ocp_finalproject.workflow.dto.SetTrendCategoryDto;
 import com.ocp.ocp_finalproject.workflow.enums.WorkflowStatus;
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
